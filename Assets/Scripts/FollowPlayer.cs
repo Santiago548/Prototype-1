d73c939a9ad724b95acc63f0ca32b024dca0f92b
Dominic Barajas <dominicbarajas548@gmail.com>
2021-11-09 18:32:32 -0700

    add to player controller variable for speed to avoid hardcoding speed. add component to main camera to follow the car at a constant rate of the player postion which is the car

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //offset the camera behind the player by adding to the players postion
+        transform.position = player.transform.position + new Vector3(0, 5, -7);
     }
 }
